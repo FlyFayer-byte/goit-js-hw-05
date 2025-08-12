7d11b27159f55bacccea3756352a74f1d2f59c5d
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-08-12 21:19:57 +0200

    modified:   js/task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,66 @@
+// Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName. 
+// Друзі кожного користувача зберігаються у властивості friends. 
+// Якщо користувачів, у яких є такий друг немає, то функція має повернути порожній масив.
+
+const getUsersWithFriend = (users, friendName) => {
+  // Метод filter() можна використовувати для створення нового масиву з елементами, які задовольняють певну умову.
+  // Використовуй метод includes() для перевірки, чи масив friends містить friendName.
+  return users.filter(user => user.friends.includes(friendName));
+};
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
